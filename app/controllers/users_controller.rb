cf854ce0d95e4a0077ffd9d0af832270bc563d33
‘stanams’ <stan.amsellem@gmail.com;GIT_COMMITTER_NAME=stanams;GIT_COMMITTER_EMAIL=stan.amsellem@gmail.com>
2016-01-25 15:28:54 -0800

    Finishes Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,41 @@
 class UsersController < ApplicationController
 
   def index
-    render json: {'a_key' => 'a value'}
+    render json: User.all
   end
 
   def create
-    render  "I'm the create method!".to_json
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render(json: user.errors.full_messages, status: :unprocessable_entity)
+    end
   end
 
   def show
-
+    render json: User.find([params[:id].to_i])
   end
+
+  def update
+    user =  User.find(params[:id])
+    user.update(user_params)
+    render json: User.find(params[:id])
+  end
+
+  def destroy
+    user =  User.find(params[:id])
+    user.delete
+    render json: User.all
+  end
+
+
+
+private
+def user_params
+   params.require(:user).permit(:name, :email)
+end
+
+
 
 end
--- bin/my_script.rb
+++ bin/my_script.rb
@@ -18,17 +18,49 @@ require 'rest-client'
 #
 # puts RestClient.post(url)
 
-url = Addressable::URI.new(
-  scheme: 'http',
-  host: 'localhost',
-  port: 3000,
-  path: '/users/5.json',
-  query_values: {
-    'some_category[a_key]' => 'another value',
-    'some_category[a_second_key]' => 'yet another value',
-    'some_category[inner_inner_hash][key]' => 'value',
-    'something_else' => 'aaahhhhh'
-  }
-).to_s
+def create_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.json'
+  ).to_s
+  puts RestClient.post(
+  url,
+  { user: { name: "Teddy", email: "cat@gmail.com" } }
+)
+end
 
-puts RestClient.get(url)
+# begin
+  # create_user
+# rescue :unprocessable_entity
+#   puts "can't create!"
+# end
+
+def update_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users/4'
+  ).to_s
+  puts RestClient.put(
+  url,
+  { user: {name: "Ben", email: "cat@gmail.com" } }
+)
+end
+
+def delete_user
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users/2'
+  ).to_s
+  puts RestClient.delete(
+  url
+  # { user: {name: "Ben", email: "cat@gmail.com" } }
+)
+end
+
+delete_user
